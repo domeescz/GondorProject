6a842c8b794818cec28e63be244de8eb97c189ec
Jan Bartyzal <jan.bartyzal@gmail.com>
2023-11-26 09:16:37 +0100

    Change base templates

--- web_app/Code/index.php
+++ web_app/Code/index.php
@@ -1,25 +1,4 @@
 <?php
-define('IN_PHPBB', true);
-session_start();
- header("Cache-control: private");
- if (!isset($_SESSION["user_is_logged"])) $_SESSION["user_is_logged"]=0;
-  $path=$_SERVER['REQUEST_URI'];
-  
-  $path1=str_replace("index.php","",$path);
-  $_SESSION["serverURI"]=$path1;
-     
- include("templates/header.php");
-
-
- ?>
-<div class="row m-1">
-<div class="col" id="content">
-
-</div>
-</div>
-
-
-<?php
-include("templates/footer.php");
-?>
- 
+header("Location:http://20.218.156.208:8080/home");
+exit();
+?> 
