053bba1dd12d9d41cd5c06aadc19021844dbe082
Dominik Šmída <dodalovilp@seznam.cz>
2023-11-30 20:33:26 +0100

    Přidání role managementu

--- web_app/src/Controller/RolesController.php
+++ web_app/src/Controller/RolesController.php
@@ -13,21 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class RolesController extends AbstractController
 {
-    #[Route('/uzivatel/{id}/roles', name: 'uzivatel_show')]
-    public function show(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $uzivatel = $entityManager->getRepository(Uzivatel::class)->find($id);
-
-        if(!$uzivatel){
-            $roles = [];
-        }
-
-        return $this->render('roles/index.html.twig', [
-            'uzivatel' => $uzivatel
-        ]);
-        #return new Response('Check the roles: '.$roles->getRoles());
-    }
-
     #[Route('/uzivatel/{id}/edit', name: 'uzivatel_edit')]
     public function edit(EntityManagerInterface $entityManager, Request $request, int $id): Response
     {
