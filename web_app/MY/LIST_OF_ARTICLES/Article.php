fdd8de5b82d0d990dd5d77732d269004f64a2973
GandalfGrey11 <148286576+GandalfGrey11@users.noreply.github.com>
2023-12-22 01:38:20 +0100

    Add files via upload

---
+++ web_app/MY/LIST_OF_ARTICLES/Article.php
@@ -0,0 +1,40 @@
+<?php
+
+// src/Entity/Article.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    // Getters and setters...
+}
---
+++ web_app/MY/LIST_OF_ARTICLES/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Controller/ArticleController.php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="article_list")
+     */
+    public function list(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
