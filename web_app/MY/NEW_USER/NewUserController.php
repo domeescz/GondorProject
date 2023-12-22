7462a71c9281fae7e1bd19bcb2d3dacd3df01323
GandalfGrey11 <148286576+GandalfGrey11@users.noreply.github.com>
2023-12-22 01:39:24 +0100

    Add files via upload

---
+++ web_app/MY/NEW_USER/NewUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Controller/NewUserController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewUserController extends AbstractController
+{
+    /**
+     * @Route("/new/user", name="new_user")
+     */
+    public function index(): Response
+    {
+        return $this->render('new_user/index.html.twig', [
+            'controller_name' => 'NewUserController',
+        ]);
+    }
+}
